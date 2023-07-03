9a5554cdba298bbadc9de3053f4819e6b3538829
Lucas_Rocha_Ribeiro <lucasrocha@frwk.com.br>
2023-07-03 14:00:01 -0300

    Criação do banco de dados no SQL Server Instalado o pacote do Dapper e o pacote de injeção de dependencia criado projeto de repository, a classe ClienteRepository e a interface IClienteRepository Feita conexão com o banco localmente Refatoração do codigo para buscar e armazenar dados no banco conforme pedido na atividade

--- Projetos PDI/README.md
+++ Projetos PDI/README.md
@@ -1,1 +1,17 @@
 Meu primeiro projeto
+
+SCRIPT DB
+
+Create database BancoFramework
+Go
+
+USE BancoFramework
+
+Create table dbo.Cliente
+(
+	Id int not null,
+	Nome varchar(30) not null,
+	CPF varchar(10) not null,
+	Saldo float not null,
+	constraint Pk_Pessoa primary key clustered (Id ASC)
+);
---
+++ Projetos PDI/Repository/IClienteRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public interface IClienteRepository
+    {
+        public Cliente Get(int id);
+        public void Insert(Cliente cliente);
+        public void Update(Cliente cliente);
+        
+    }
+}
