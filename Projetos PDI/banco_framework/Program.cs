03884cb9aef8c4822c92a5c26c3bbfd509b13bc3
Lucas_Rocha_Ribeiro <lucasrocha@frwk.com.br>
2023-10-23 10:55:02 -0300

    Codigo refatorado para melhor leitura,  corrigido problemas em caso de inserção de valores negativos em deposito, saque e no momento de criação do saldo de um novo cliente, removidos os using que não estavam sendo utilizados, corrigido caso em que o cliente era salvo mesmo com cpf incorreto.

--- Projetos PDI/banco_framework/Program.cs
+++ Projetos PDI/banco_framework/Program.cs
@@ -1,18 +1,18 @@
 ﻿using Application;
-using Domain.Model;
 using CpfCnpjLibrary;
-using Repository;
+using Domain.Model;
 using Microsoft.Extensions.DependencyInjection;
+using Repository;
 
 internal class Program
 {
-    private IClienteRepository _cliente;
     public static void Main(string[] args)
     {
         var serviceCollection = new ServiceCollection();
         ConfigureServices(serviceCollection);
         var serviceProvider = serviceCollection.BuildServiceProvider();
         var clienteService = serviceProvider.GetService<IClienteRepository>();
+
         Console.Clear();
         Console.WriteLine("Seja bem vindo ao banco Framework");
         Console.WriteLine("Por favor, identifique-se");
@@ -20,53 +20,48 @@ internal class Program
         var cliente = Identificacao(clienteService);
         
         int opcao = 0;
-        var calculo = new Calculo();
+        var calculadora = new Calculo();
 
         while (opcao != 3)
         {
             opcao = Menu(cliente, opcao);
-
+            
             switch (opcao)
             {
                 case 1:
-                    Console.WriteLine("Depósito");
-                    var deposito = float.Parse(Console.ReadLine());
-                    cliente.Saldo = calculo.Soma(cliente.Saldo, deposito);
-                    clienteService.Update(cliente);
-                    Console.WriteLine($"Saldo atual é {clienteService.Get(cliente.Id).Saldo.ToString("N2")}");
-                    Console.ReadLine();
+                    Deposito(cliente, calculadora, clienteService);
                     break;
                 case 2:
-                    Console.WriteLine("Saque");
-                    var saque = float.Parse(Console.ReadLine());
-                    cliente.Saldo = calculo.Subtracao(cliente.Saldo, saque);
-                    clienteService.Update(cliente);
-                    Console.WriteLine($"Saldo atual é {clienteService.Get(cliente.Id).Saldo.ToString("N2")}");
-                    Console.ReadLine();
+                    Saque(cliente, calculadora, clienteService);
                     break;
             }
         }
         
     }
+
     static void ConfigureServices(IServiceCollection services)
     {
         services.AddScoped<IClienteRepository, ClienteRepository>();
     }
+
     static Cliente Identificacao(IClienteRepository clienteService)
     {
         var cliente = new Cliente();
 
-        List<string> erros = new List<string>();
-        erros.Add(" ");
-        while (erros.Count() != 0)
+        List<string> errosCount = new List<string>();
+        errosCount.Add(" ");
+
+        while (errosCount.Count() != 0)
         {
-            erros.Clear();
+            errosCount.Clear();
 
             Console.WriteLine("Seu número de identificação:");
+
             var id = Console.ReadLine();
+
             if (!int.TryParse(id, out _))
             {
-                erros.Add("Identificador não é válido");
+                errosCount.Add("Identificador não é válido");
             }
             else
             {
@@ -77,42 +72,25 @@ internal class Program
                     return client;
             }
 
-            Console.WriteLine("Seu nome:");
-            cliente.Nome = Console.ReadLine();
-
-            Console.WriteLine("Seu CPF:");
-            cliente.Cpf = Console.ReadLine();
-            if (Cpf.Validar(cliente.Cpf).Equals(false))
-            {
-                erros.Add("CPF digitado não é válido");
-            }
-
-            Console.WriteLine("Seu Saldo:");
-            var saldo = Console.ReadLine();
-            if (!float.TryParse(saldo, out _))
-            {
-                erros.Add("Saldo não é válido");
-            }
-            else
-            {
-                cliente.Saldo = float.Parse(saldo);
-            }
+            IdentificacaoNovoCliente(cliente, int.Parse(id), clienteService, errosCount);
 
             Console.WriteLine();
 
-            if (erros.Count() > 0)
+            if (errosCount.Count() > 0)
             {
-                foreach (string mensagem in erros)
+                foreach (string erro in errosCount)
                 {
-                    Console.WriteLine(mensagem);
+                    Console.WriteLine(erro);
                 }
-                Console.ReadLine();
             }
-            
-            clienteService.Insert(cliente);
+
+            Console.ReadKey();
 
             Console.Clear();
         }
+
+        clienteService.Insert(cliente);
+
         return cliente;
     }
 
@@ -136,4 +114,136 @@ internal class Program
         return opcao;
     }
 
+    static Cliente IdentificacaoNovoCliente(Cliente cliente, int id, IClienteRepository clienteService, List<string> erros)
+    {
+        cliente.Id = id;
+           
+        Console.WriteLine("Seu nome:");
+        cliente.Nome = Console.ReadLine();
+
+        ClienteCpf(cliente, clienteService, erros);
+
+        Saldo(cliente, clienteService, erros);
+
+        return cliente;
+    }
+
+    static Cliente ClienteCpf(Cliente cliente, IClienteRepository clienteService, List<string> erros)
+    {
+        Console.WriteLine("Seu CPF:");
+
+        cliente.Cpf = Console.ReadLine();
+
+        cliente.Cpf = Cpf.FormatarSemPontuacao(cliente.Cpf);
+
+        if (Cpf.Validar(cliente.Cpf).Equals(false))
+        {
+            Erros(erros, "CPF digitado não é válido");
+        }
+
+        return cliente;
+    }
+
+    static Cliente Saldo(Cliente cliente, IClienteRepository clienteService, List<string> erros)
+    {
+        Console.WriteLine("Seu Saldo:");
+
+        var saldo = Console.ReadLine();
+
+        if (!float.TryParse(saldo, out _))
+        {
+            Erros(erros, "Saldo não é válido");
+        }
+        else
+        {
+            var conversaoSaldo = float.Parse(saldo);
+
+            if (conversaoSaldo < 0)
+                Erros(erros, "Saldo não é válido");
+            else
+                cliente.Saldo = conversaoSaldo;
+        }
+
+        return cliente;
+    }
+
+    static Cliente Deposito(Cliente cliente, Calculo calculadora, IClienteRepository clienteService)
+    {
+        Console.WriteLine("Depósito");
+
+        Console.Write("Digite o valor: ");
+
+        var deposito = Console.ReadLine();
+
+        if (!float.TryParse(deposito, out _))
+        {
+            Console.WriteLine("Valor digitado é invalido: digite um valor numerico");
+            Console.ReadKey();
+            return cliente;
+        }
+        else
+        {
+            var valorConvertido = float.Parse(deposito);
+
+            if (valorConvertido < 0)
+            {
+                Console.WriteLine("Valor digitado é invalido: digite um valor positivo");
+                Console.ReadKey();
+                return cliente;
+            }
+            else
+                cliente.Saldo = calculadora.Soma(cliente.Saldo, valorConvertido);
+        }
+
+        clienteService.Update(cliente);
+
+        Console.WriteLine($"Saldo atual é {clienteService.Get(cliente.Id).Saldo.ToString("N2")}");
+
+        Console.ReadLine();
+
+        return cliente;
+    }
+
+    static Cliente Saque(Cliente cliente, Calculo calculadora, IClienteRepository clienteService)
+    {
+        Console.WriteLine("Saque");
+
+        Console.Write("Digite o valor: ");
+
+        var saque = Console.ReadLine();
+
+        if (!float.TryParse(saque, out _))
+        {
+            Console.WriteLine("Valor digitado é invalido: digite um valor numerico");
+            Console.ReadKey();
+            return cliente;
+        }
+        else
+        {
+            var valorConvertido = float.Parse(saque);
+
+            if (valorConvertido < 0)
+            {
+                Console.WriteLine("Valor digitado é invalido: digite um valor positivo");
+                Console.ReadKey();
+                return cliente;
+            }
+            else
+                cliente.Saldo = calculadora.Subtracao(cliente.Saldo, valorConvertido);
+        }
+
+        clienteService.Update(cliente);
+
+        Console.WriteLine($"Saldo atual é {clienteService.Get(cliente.Id).Saldo.ToString("N2")}");
+
+        Console.ReadLine();
+
+        return cliente;
+    }
+
+    static List<string> Erros(List<string> erros, string erro)
+    {
+        erros.Add(erro);
+        return erros;
+    }
 }
